745e0fd0a45837fbf192f2ca0094ea21b989846e
加藤公彦 <kimihiko@katoukimihiko-no-MacBook-Pro.local>
2017-09-16 19:36:07 +0900

    make it better

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,6 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
 
    def index
     @picture = Picture.all
