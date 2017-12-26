2002112f3a70bced12f279fd05fe9f0ff61217ee
hiko211 <hiko211@gmail.com>
2017-12-26 14:20:46 +0900

    mailer周り

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  #if Rails.env.development?
+  #  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  #end
 
 
   # resources :blogs, only: [:index, :show, :new, :create, :edit, :update ,:destroy] do
